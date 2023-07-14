b237d42022bdcf09d4e30cb9b92b7c2d5a1b8551
textil24 <textilofficial@mail.ru>
2023-07-14 16:39:21 +0300

    Added ApolloServerPluginLandingPageGraphQLPlayground

--- src/index.ts
+++ src/index.ts
@@ -5,12 +5,16 @@ import { startStandaloneServer } from "@apollo/server/standalone"
 import { typeDefs } from "./schema/index.js"
 import { resolvers } from "./resolvers/index.js"
 import { PrismaClient } from "@prisma/client"
+import { ApolloServerPluginLandingPageGraphQLPlayground } from "@apollo/server-plugin-landing-page-graphql-playground"
 
 const prisma = new PrismaClient()
 
 const API_PORT = 4001
 
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ 
+    plugins: [ApolloServerPluginLandingPageGraphQLPlayground()],
+    typeDefs, 
+    resolvers })
 
 async function main() {
     const { url } = await startStandaloneServer(server, {
